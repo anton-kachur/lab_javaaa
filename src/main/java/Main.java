1d5c03bb6545b8ffdbd6d4253d828980ac77f376
antik1420@gmail.com <antik1420@gmail.com>
2020-11-09 09:18:30 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MainManager mainManager = new MainManager();
+
+        while (true) {
+            mainManager.interactiveMenu();
+            mainManager.start();
+        }
+    }
+}
+
+
